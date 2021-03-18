9232fb1c6ee1285ceb1a636f28d7c716485618dc
FANDRADE18 <fandradetv@gmail.com>
2021-03-18 06:10:05 -0600

    v1 login veterinaria

---
+++ app/src/main/java/com/fandrade/veterinaria/MainActivity.java
@@ -0,0 +1,23 @@
+package com.fandrade.veterinaria;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+ EditText user,pass;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
